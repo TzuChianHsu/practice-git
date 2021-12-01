e43ade757d353e3db300f343fdc905b0a84c8402
Tzu,chien Hsu <>
2021-12-01 17:21:56 +0800

    add btn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+// next.config.ts
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withLess = require('next-with-less');
+
+const plugins = [
+  /* ...other plugins... */
+  [
+    withLess,
+    {
+      lessLoaderOptions: {
+        lessOptions: {
+          modifyVars: {
+            'primary-color': '#00ccb4',
+            'border-radius-base': '4px',
+          },
+        },
+      },
+    },
+  ],
+  /* ...other plugins... */
+];
+
+module.exports = withPlugins(plugins, {
   reactStrictMode: true,
   images: {
-    domains: ["fakestoreapi.com"],
+    domains: ['fakestoreapi.com'],
   },
-};
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
+import "antd/dist/antd.less";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
